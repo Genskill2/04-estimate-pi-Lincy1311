0608478ce0b86add38f501647e07bfd713183257
Lincy1311 <71397922+Lincy1311@users.noreply.github.com>
2021-06-03 15:51:35 +0530

    Update wallis.c

--- wallis.c
+++ wallis.c
@@ -1,14 +1,56 @@
-#include<stdio.h>
+#include <assert.h>
 
-int main()
+#include <stdlib.h>
 
-{  int n,i;
+#include <stdio.h>
+
+#include <math.h>
+
+float wallis_pi(int);
+
+int main(void) {
+
+  float pi;
+
+  for (int i=0; i<5; i++) {
+
+    pi = wallis_pi(i);
+
+    if (!(fabs(pi - M_PI) > 0.15)) {
+
+      printf("Estimate with just %d iterations is %f which is too accurate.\n", i, pi);
+
+      abort();
+
+    }
+
+  }
+
+  for (int i=500; i<3000; i++) {
+
+    pi = wallis_pi(i);
+
+    if (!(fabs(pi - M_PI) < 0.01)) {
+
+      printf("Estimate with even %d iterations is %f which is not accurate enough.\n", i, pi);
+
+      abort();
+
+    }
+
+  }
+
+}
+
+int main(int n)
+
+{  int i;
 
    float temp,val,sum=1;
 
-   printf("enter n");
+   
 
-   scanf("%d",&n);
+
 
    for(i=1;i<=n;i++)
 
@@ -22,7 +64,19 @@ int main()
 
    printf("%.2f",val);
 
-   
- 
+   return 0;
 
 }
+
+
+
+   
+
+   
+
+
+
+   
+
+   
+
