6d53da721b4ac47862a5f9122f6b6cc0a7d9087a
Lincy1311 <71397922+Lincy1311@users.noreply.github.com>
2021-06-03 11:30:59 +0530

    Update wallis.c

--- wallis.c
+++ wallis.c
@@ -5,6 +5,7 @@
 
 float wallis_pi(int);
 
+
 int main(void) {
   float pi;
   for (int i=0; i<5; i++) {
@@ -24,3 +25,28 @@ int main(void) {
   }
 }
 
+float wallis_pi(int n)
+
+{  int i;
+
+   float temp,val,sum=1;
+
+   printf("enter n");
+
+   scanf("%d",&n);
+
+   for(i=1;i<=n;i++)
+
+   {  temp=(4*i*i)/((4*i*i)-1.0);
+
+      sum=sum*temp;
+
+   }
+
+   val=2*sum;
+
+   printf("%.2f",val);
+
+   return 0;
+
+}
