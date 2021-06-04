4deae3bf2128165fc21326efb2a21a0a3acea7ea
Lincy1311 <71397922+Lincy1311@users.noreply.github.com>
2021-06-04 09:39:08 +0530

    Create monte_carlo.c

--- monte_carlo.c
+++ monte_carlo.c
@@ -5,36 +5,6 @@
 
 float mc_pi(int);
 
-
-
-
-
-
-
-
-   
-
-
-      
-
-
-
-      
-
-      
-
-  
-
-    
-
-    
-
-
-
-
-
-
-
 float frandom() {
   long int q = random();
   float ret = (float)q/(float)RAND_MAX;
@@ -72,33 +42,19 @@ int main(void) {
 float mc_pi(int n)
 
 { int circle = 0;
-
-   int square = 0;
-
-   float x,y;
-
-   for(int i=1;i<=n;i++)
-
-   { x=frandom();
-
-      y=frandom();
-
-      if(((x*x)+(y*y))<=1)
-
-      {   circle++;    }
-
-      else
-
-      {   square++;  }
-
-    }
-
-    square=square + circle;
-
-    float pi = 4*(float)circle/square;
-
-    return pi;
-
+  int square = 0;
+  float x,y;
+  for(int i=1;i<=n;i++)
+  { x=frandom();
+    y=frandom();
+    if(((x*x)+(y*y))<=1)
+    {   circle++;    }
+    else
+    {   square++;   }
+  }
+  square=square + circle;
+  float pi = 4*(float)circle/square;
+  return pi;
 }
 
 
