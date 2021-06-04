b98db6f74631a1fabe8f5eb4b22b892e36dab16e
Lincy1311 <71397922+Lincy1311@users.noreply.github.com>
2021-06-04 11:36:21 +0530

    Update wallis.c

--- wallis.c
+++ wallis.c
@@ -37,14 +37,13 @@ int main(void) {
 
 }
 
-float wallis_pi(int n)
-
+float wallis_pi(int count)
 {  
    float pi=1.0;
-   for(int i=1;i<n;i++)
+   for(int i=1;i<count;i++)
    {  
-      float num = 4.0*i*i;
-     pi* = num/(num-1);
+     float num = 4.0*i*i;
+     pi*=num/(num-1);
 
    }
    
